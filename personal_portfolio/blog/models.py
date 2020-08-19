e6049836357658e9f9f99a74abce22687eaa4b3a
mukund2124 <mukundkushwaha2124@gmail.com>
2020-08-19 22:43:36 +0545

    trying

--- personal_portfolio/blog/models.py
+++ personal_portfolio/blog/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=600)
+    date = models.DateField()
--- personal_portfolio/blog/views.py
+++ personal_portfolio/blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Blog
 
-# Create your views here.
+def all_blog(request):
+    blogs = Blog.objects.order_by('-date')[:4]
+    return render(request, 'blog/all_blog.html', {'blogs':blogs})
+
